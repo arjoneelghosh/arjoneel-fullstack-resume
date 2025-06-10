784ea49ff07d23de4da0e6f75eaa062831a65141
Arjoneel Ghosh <arjoneelghosh03@gmail.com>
2025-06-10 22:38:48 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import { Projects } from './pages/Projects';
 
 function App() {
   return (
+    #d
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
       <Navigation />
       <Routes>
